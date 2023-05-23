619e7dbed2bdadb3c3fe681d233ec43f72cf4758
huijiang <57055520+354169679@users.noreply.github.com>
2023-05-23 16:58:36 +0800

    Update Epoll.h
    调整为引用

--- Epoll.h
+++ Epoll.h
@@ -27,29 +27,29 @@ public:
         errif(epoll_fd_ == -1, "epoll create error");
     }
 
-    void del_channel(Channel *ch)
+    void del_channel(Channel::ChannelPtr &ch)
     {
         int channel_fd_ = ch->get_channel_fd();
         int err = epoll_ctl(epoll_fd_, EPOLL_CTL_DEL, channel_fd_, nullptr);
         errif(err == -1, "epoll delete fd error");
     }
 
-    void add_channel(Channel *ch)
+    void add_channel(Channel::ChannelPtr &ch)
     {
         int channel_fd_ = ch->get_channel_fd();
         epoll_event *ev = new epoll_event;
-        ev->data.ptr = ch;
+        ev->data.ptr = ch.get();
         ev->events = ch->get_interest_event();
         int err = epoll_ctl(epoll_fd_, EPOLL_CTL_ADD, channel_fd_, ev);
         errif(err == -1, "epoll delete fd error");
         delete ev;
     }
 
-    void mod_channel(Channel *ch)
+    void mod_channel(Channel::ChannelPtr &ch)
     {
         int channel_fd_ = ch->get_channel_fd();
         epoll_event *ev = new epoll_event;
-        ev->data.ptr = ch;
+        ev->data.ptr = ch.get();
         ev->events = ch->get_interest_event();
         int err = epoll_ctl(epoll_fd_, EPOLL_CTL_MOD, channel_fd_, ev);
         errif(err == -1, "epoll delete fd error");
@@ -71,7 +71,6 @@ again:
                 continue;
             auto ch = static_cast<Channel *>(coming_ev[i].data.ptr);
             ch->set_happend_event(coming_ev[i].events);
-            spdlog::info("push back");
             list.push_back(ch);
         }
     }
