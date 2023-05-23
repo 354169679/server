8dc8d930d510755a4e9528b6fa3120a37b966988
huijiang <57055520+354169679@users.noreply.github.com>
2023-05-23 08:26:19 +0800

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -1,7 +1,23 @@
 
-#include "SerSock.hpp"
-#include "Channel.hpp"
-#include "EventLoop.hpp"
+#include "SerSock.h"
+#include "Channel.h"
+#include "EventLoop.h"
+
+#include "spdlog/spdlog.h"
+
+void client_cb(EventLoop *loop, Channel *ch)
+{
+    char buf[20] = {0};
+    // read(ch->get_channel_fd(), buf, sizeof(buf));
+    std::cout << "hello client!" << std::endl;
+}
+
+void listen_cb(EventLoop *loop, Channel *ch)
+{
+    spdlog::info("accept a connection require");
+    int client_fd = accept4(ch->get_channel_fd(), nullptr, nullptr, O_NONBLOCK);
+    loop->add_event(client_fd, client_cb, EPOLLIN);
+}
 
 int main()
 {
@@ -14,8 +30,10 @@ int main()
     server_socket.bind();
     server_socket.listen();
 
-    server_socket.accept();
-    
+    loop.add_event(listen_fd, listen_cb, EPOLLIN);
+
+    spdlog::info("start loop");
+    loop.start();
 
     return 0;
 }
