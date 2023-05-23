c1e9551ad2c5f9d430d69d8de8997a7afa942e74
huijiang <57055520+354169679@users.noreply.github.com>
2023-05-23 08:25:23 +0800

    Update Epoll.h

--- Epoll.h
+++ Epoll.h
@@ -3,16 +3,16 @@
 #include <sys/epoll.h>
 #include <unistd.h>
 
-
 #include <iostream>
-#include <unordered_set>
 
 #include "Channel.h"
 #include "util.h"
 
-
+#include "spdlog/spdlog.h"
 
 constexpr int MAX_COUNT = 100;
+
+class Channel;
 
 class Epoll
 {
@@ -27,13 +27,54 @@ public:
         errif(epoll_fd_ == -1, "epoll create error");
     }
 
-    void del_channel(Channel *ch);
+    void del_channel(Channel *ch)
+    {
+        int channel_fd_ = ch->get_channel_fd();
+        int err = epoll_ctl(epoll_fd_, EPOLL_CTL_DEL, channel_fd_, nullptr);
+        errif(err == -1, "epoll delete fd error");
+    }
 
-    void add_channel(Channel *ch);
+    void add_channel(Channel *ch)
+    {
+        int channel_fd_ = ch->get_channel_fd();
+        epoll_event *ev = new epoll_event;
+        ev->data.ptr = ch;
+        ev->events = ch->get_interest_event();
+        int err = epoll_ctl(epoll_fd_, EPOLL_CTL_ADD, channel_fd_, ev);
+        errif(err == -1, "epoll delete fd error");
+        delete ev;
+    }
 
-    void mod_channel(Channel *ch);
+    void mod_channel(Channel *ch)
+    {
+        int channel_fd_ = ch->get_channel_fd();
+        epoll_event *ev = new epoll_event;
+        ev->data.ptr = ch;
+        ev->events = ch->get_interest_event();
+        int err = epoll_ctl(epoll_fd_, EPOLL_CTL_MOD, channel_fd_, ev);
+        errif(err == -1, "epoll delete fd error");
+        delete ev;
+    }
 
-    void poll(Channel::ChannelList &list);
+    void poll(std::vector<Channel *> &list)
+    {
+        int err = epoll_wait(epoll_fd_, coming_ev, MAX_COUNT, -1);
+        errif(err == -1, "epoll wait error");
+again:
+        if (errno == EAGAIN)
+        {
+            goto again;
+        }
+        for (int i = 0; i < err; ++i)
+        {
+            if (coming_ev[i].data.ptr == nullptr)
+                continue;
+            auto ch = static_cast<Channel *>(coming_ev[i].data.ptr);
+            ch->set_happend_event(coming_ev[i].events);
+            spdlog::info("push back");
+            list.push_back(ch);
+        }
+    }
 
     ~Epoll()
     {
