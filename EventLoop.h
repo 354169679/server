8ca20dc90159b41a1fa3325f1563f91e50c33da5
huijiang <57055520+354169679@users.noreply.github.com>
2023-05-23 08:25:40 +0800

    Update EventLoop.h

--- EventLoop.h
+++ EventLoop.h
@@ -1,52 +1,68 @@
 #pragma once
 
 #include <memory>
-#include <unordered_set>
+#include <functional>
+#include <vector>
+
+#include "NoCopy.h"
+#include "Epoll.h"
+
+#include "spdlog/spdlog.h"
 
 
-class Epoll;
 
-class EventLoop
+class EventLoop : NoCopy
 {
 private:
-    Epoll *ep_;
-    std::unordered_set<std::unique_ptr<Channel>> list_;
+    std::unique_ptr<Epoll> epoll_ptr;
+    Channel::ChannelList list_;
+    std::vector<Channel*> active_list_;
     bool quit_;
 
-    inline void add_channel_to_list(std::unique_ptr<Channel> &&channel_ptr)
+    inline void add_channel_to_list(Channel::ChannelPtr &&channel_ptr)
     {
         list_.insert(std::move(channel_ptr));
     }
 
 public:
-    EventLoop() = default;
+    EventLoop(Epoll *ep = nullptr) : epoll_ptr(std::make_unique<Epoll>()), quit_(false) {}
     ~EventLoop() = default;
 
-    EventLoop(Epoll *ep) : ep_(ep), quit_(false) {}
-
-    void add_event(int fd, uint32_t ev, const std::function<void()> &cb)
+    /// @brief 添加fd回调任务
+    /// @param fd 关注的文件描述符
+    /// @param cb 文件描述符所绑定的回调函数
+    /// @param ev 文件描述符所关注的事件类型
+    void add_event(int fd, const Channel::EventCbFun &cb, uint32_t ev)
     {
-        std::unique_ptr<Channel> channel_ptr = std::make_unique<Channel>(fd, ev, cb, this);
-        if (channel_ptr == nullptr)
-            quit();
+        Channel::ChannelPtr channel_ptr = std::make_unique<Channel>(this, fd, ev);
+        errif(channel_ptr == nullptr, "channel ptr is nullptr");
+        channel_ptr->set_cb(cb);
+        epoll_ptr->add_channel(channel_ptr.get());
         add_channel_to_list(std::move(channel_ptr));
     }
 
-    inline void quit()
+    void add_timer_event(const std::function<void> &cb)
+    {
+
+    }
+
+    void quit()
     {
         quit_ = true;
     }
 
-    // void loop()
-    // {
-    //     while (!quit_)
-    //     {
-    //         list_.clear();
-    //         ep_->poll(list_);
-    //         for (auto &i : list_)
-    //         {
-    //             i->handle_event();
-    //         }
-    //     }
-    // }
+    void start()
+    {
+        while (!quit_)
+        {
+            active_list_.clear();
+            epoll_ptr->poll(active_list_);
+            spdlog::info("get epoll,sum active list:{}",active_list_.size());
+            sleep(1);
+            for (auto &i:active_list_)
+            {
+                i->handle_event();
+            }
+        }
+    }
 };
