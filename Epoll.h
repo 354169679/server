83ac6194eaae6949af1bbb325c2c020a8486b8aa
huijiang <57055520+354169679@users.noreply.github.com>
2023-05-24 15:11:55 +0800

    Update Epoll.h
    不使用c-style数组，换为array。

--- Epoll.h
+++ Epoll.h
@@ -4,6 +4,7 @@
 #include <unistd.h>
 
 #include <iostream>
+#include <array>
 
 #include "Channel.h"
 #include "util.h"
@@ -21,51 +22,58 @@ public:
 
 private:
     int epoll_fd_;
-    epoll_event coming_ev[MAX_COUNT];
+    epoll_event ev;
+    // epoll_event coming_ev[MAX_COUNT];
+    std::array<epoll_event, MAX_COUNT> coming_ev;
 
 private:
-    void operate_epoll(Channel::ChannelPtr &ch, int op)
+    void operate_epoll(const Channel::ChannelPtr &ch, int op)
     {
+        bzero(&ev, sizeof(ev));
         int channel_fd_ = ch->get_channel_fd();
-        epoll_event *ev = new epoll_event;
-        ev->data.ptr = ch.get();
-        ev->events = ch->get_interest_event();
-        int err = epoll_ctl(epoll_fd_, op, channel_fd_, ev);
+        ev.data.ptr = ch.get();
+        ev.events = ch->get_interest_event();
+        int err = epoll_ctl(epoll_fd_, op, channel_fd_, &ev);
         errif(err == -1, "epoll delete fd error");
-        delete ev;
     }
 
 public:
-    Epoll() : epoll_fd_(-1)
+    Epoll() : epoll_fd_(epoll_create1(EPOLL_CLOEXEC))
     {
-        epoll_fd_ = epoll_create1(0);
+        bzero(&ev, sizeof(ev));
         errif(epoll_fd_ == -1, "epoll create error");
     }
 
-    void del_channel(Channel::ChannelPtr &ch) // 只能使用引用或者指针，因为调用del_channel隐式使用了拷贝构造函数，ChannelPtr为unique_ptr无法被拷贝
+    void del_channel(const Channel::ChannelPtr &ch) // 只能使用引用或者指针，因为调用del_channel隐式使用了拷贝构造函数，ChannelPtr为unique_ptr无法被拷贝
     {
         operate_epoll(ch, EPOLL_CTL_DEL);
     }
 
-    void add_channel(Channel::ChannelPtr &ch)
+    void add_channel(const Channel::ChannelPtr &ch)
     {
         operate_epoll(ch, EPOLL_CTL_ADD);
     }
 
-    void mod_channel(Channel::ChannelPtr &ch)
+    void mod_channel(const Channel::ChannelPtr &ch)
     {
         operate_epoll(ch, EPOLL_CTL_MOD);
     }
 
     void poll(std::vector<Channel *> &list)
     {
-        int err = epoll_wait(epoll_fd_, coming_ev, MAX_COUNT, -1);
-        // errif(err == -1, "epoll wait error");
     again:
+        coming_ev.fill(epoll_event());
+        int err = epoll_wait(epoll_fd_, coming_ev.data(), MAX_COUNT, -1);
+
         if (errno == EAGAIN || errno == EINTR)
         {
             goto again;
         }
+        else
+        {
+            errif(err == -1, "epoll wait error");
+        }
+        
         for (int i = 0; i < err; ++i)
         {
             if (coming_ev[i].data.ptr == nullptr)
